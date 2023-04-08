a3b40a4502af37cc1716c259731c562d7f82a43d
igor <altilan.mne@gmail.com>
2023-04-08 18:31:29 +0200

    Commit 1. Chrome to JSON method added.

--- src/bmconv.py
+++ src/bmconv.py
@@ -2,18 +2,30 @@
 Convert the format of storage files as JSON, SOLite and more.
 Use the command line for the user interface.
 Implemented with the built-in argparse library.
+Convert all external formats into base internal JSON format.
+Converting from external formats to the other internals provides
+as cross-formatting via JSON format.
 
 """
-
+import os
 import argparse
 from functools import partial
 import json
+import typing as t
+
+from my_nodes import RootBookmarks
+from my_nodes import Folder
+from my_nodes import Url
+from time_convert import stamp_to_string
+
 
 
 # common data
 version = 'BM file converter 2.0'
 list_input_formats = ['chrome', 'json', 'sqlite']
 list_output_formats = ['json', 'sqlite']
+
+
 
 class MyJSONEncoder(json.JSONEncoder):
     """Overwrite the default JSON encoder class from the json module
@@ -34,81 +46,108 @@ class MyJSONEncoder(json.JSONEncoder):
         else:
             super().default(obj)  # the object does not need to be transformed
 
-class JsonTree:
-    """A class of a internal bookmark structure using JSON format to store data.
+class ChromeToJSON:
+    """A class to convert Chrome bookmark (JSON) format into internal JSON format.
     """
 
+    def __init__(self):
+        """Constructor method.
+        """
+        self.root = RootBookmarks()  # create a new bookmark's tree object
+        self.root.nodes_dict['roots'] = self.root  # {'roots': self.root object}  is the first record to the nodes dict
 
+    # ---- section of format conversions
+    def _chrome_into_object(self, dt: dict) -> dict:
+        """Conversion of the Chrome JSON structure to the internal node tree.
+        Inner recursion for convert_chrome method, in-place conversion
 
-# ---- section of format conversions
-def _chrome_into_object(self, dt: dict) -> dict:
-    """Conversation of chrome json structure to the internal node tree.
-    Inner recursion for convert_chrome method, in-place conversion
+        :param dt: a dictionary with chrome JSON structure
+        :return: intermediate dictionary during recursion execution
+        """
+        init_len = len(dt['children'])  # an initial length of the input dict
+        while init_len:
+            attr_dict = dt['children'][0]  # get first child from the list and remove it
 
-    :param dt: a dictionary with chrome json structure
-    :return: intermediate dictionary during recursion execution
-    """
-    init_len = len(dt['children'])  # an initial length of the input dict
-    while init_len:
-        attr_dict = dt['children'][0]  # get first child from the list and remove it
+            # remove non-using data fields of chrome format
+            attr_dict.pop('meta_info', None)  # remove a <meta_info> list if it presents
 
-        # remove non-using data fields of chrome format
-        attr_dict.pop('meta_info', None)  # remove a <meta_info> list if it presents
-
-        # modify source structure to internal
-        attr_dict['name'] = self.root.duplicate_name(attr_dict['name'])  # replace duplicate name with name(i)
-        attr_dict['id_no'] = attr_dict.pop('id')  # replace key 'id' with 'id_no' keeping its value
-        attr_dict['parent_name'] = dt['name']  # set parent name for child object
-        if attr_dict.pop('type') == 'folder':
-            node_type = True  # this is a folder
-            if attr_dict['name'] != 'roots':  # skip format conversion for roots folder
-                # convert timestamps of the folder
+            # modify source structure to internal
+            attr_dict['name'] = self.root.duplicate_name(attr_dict['name'])  # replace duplicate name with name(i)
+            attr_dict['id_no'] = attr_dict.pop('id')  # replace key 'id' with 'id_no' keeping its value
+            attr_dict['parent_name'] = dt['name']  # set parent name for child object
+            if attr_dict.pop('type') == 'folder':
+                node_type = True  # this is a folder
+                if attr_dict['name'] != 'roots':  # skip format conversion for roots folder
+                    # convert timestamps of the folder
+                    attr_dict['date_added'] = stamp_to_string(int(attr_dict['date_added']), 'google')
+                    attr_dict['date_modified'] = stamp_to_string(int(attr_dict['date_modified']), 'google')
+                self.root.add_node(attr_dict, node_type)  # call an appropriated nodes method
+                self._chrome_into_object(attr_dict)  # recursion
+            else:
+                node_type = False  # this is an url
+                # convert timestamp of the url
                 attr_dict['date_added'] = stamp_to_string(int(attr_dict['date_added']), 'google')
-                attr_dict['date_modified'] = stamp_to_string(int(attr_dict['date_modified']), 'google')
-            self.root.add_node(attr_dict, node_type)  # call an appropriated nodes method
-            self._chrome_into_object(attr_dict)  # recursion
-        else:
-            node_type = False  # this is an url
-            # convert timestamp of the url
-            attr_dict['date_added'] = stamp_to_string(int(attr_dict['date_added']), 'google')
-            self.root.add_node(attr_dict, node_type)  # call an appropriated nodes method
+                self.root.add_node(attr_dict, node_type)  # call an appropriated nodes method
 
-        # an object has been created and added
-        del dt['children'][0]  # remove a dict of the added node
-        init_len -= 1
-    return dt  # return the dict where dicts are replaced by equivalent objects - nodes
+            # an object has been created and added
+            del dt['children'][0]  # remove a dict of the added node
+            init_len -= 1
+        return dt  # return the dict where dicts are replaced by equivalent objects - nodes
 
 
-def convert_chrome(self, filename: str) -> tuple[bool, str]:
-    """Convert Chrome bookmark JSON filename to the current tree. Return (True/False, error message)
+    def convert(self, filename: str) -> t.Tuple[bool, str]:
+        """Convert Chrome bookmark JSON filename to the current tree. Fill RootBookmarks object in-place.
 
-    :param filename: Google bookmark filename to convert
-    :return: (True, empty string)  or (False, error message)
+        :param filename: Google bookmark filename to convert
+        :return: (True, empty string)  or (False, error message) and filled tree
+        """
+        # ---- open and load JSON file of Chrome bookmarks, the file definitely exists
+        with open(filename, 'r', encoding='utf-8') as f:  # open the tree image file, or raise FileNotFoundError
+            source_file = json.load(f)  # read the json image and then close the file, image is a dict
+
+        # ---- extract the roots dict only, checksum, sync_metadata and version attrs do not use
+        chrome_keys = list(source_file)  # get chrome main keys of the bookmark object
+        if 'checksum' and 'roots' and 'version' not in chrome_keys:  # if keys don't exist that is wrong file format
+            return False, f'Source file {filename} has wrong format'  # return with error message
+
+        # ---- this is a chrome bookmark file ----
+        # ---- remove unnecessary attrs from source structure: checksum, version, sync_metadata ----
+        chrome_roots = source_file['roots']  # roots names and values dictionary only - (name: root), ...
+        # ---- prepare a dict to create our internal root from source data
+        tree_image = dict()  # clear attrs dict
+        tree_image['children'] = list(chrome_roots.values())  # get a root children list, DO NOT VIEW !!!
+        self.root.update_root(**tree_image)  # update the current tree with source values
+
+        # ---- decode nested dictionaries from json image to the original objects and add it to the node's list ----
+        dt = self.root.__dict__  # start from the root children
+        self._chrome_into_object(dt)  # decode nested dictionaries recursively
+
+        return True, ''  # return success
+
+def print_verbose(message: str, verbose):
+    """Print message if verbose mode set."""
+    if verbose:
+        print(message)
+
+def chrome_to_json(source: str, destination: str):
+    """Convert Chrome bookmark to internal JSON format.
+    Create and fill internal tree then serialise and save data to a file.
+
+    :param source: filename of external Chrome bookmark (JSON format)
+    :param destination: filename of BookmarkManager internal JSON format
+    :return:
     """
-    # ---- open and load JSON file of Chrome bookmarks, the file exists
-    with open(filename, 'r', encoding='utf-8') as f:  # open the tree image file, or raise FileNotFoundError
-        source_file = json.load(f)  # read the json image and then close the file, image is a dict
-
-    # ---- extract the roots dict only, checksum, sync_metadata and version attrs do not use
-    chrome_keys = list(source_file)  # get chrome main keys of the bookmark object
-    if 'checksum' and 'roots' and 'version' not in chrome_keys:  # if keys don't exist that is wrong file format
-        return False, f'Source file {filename} has wrong format'
-
-    # ---- this is a chrome bookmark file ----
-    # ---- remove unnecessary attrs from source structure: checksum, version, sync_metadata ----
-    chrome_roots = source_file['roots']  # roots names and values dictionary only - (name: root), ...
-    # ---- prepare a dict to create our internal root from source data
-    tree_image = dict()  # clear attrs dict
-    tree_image['children'] = list(chrome_roots.values())  # get a root children list, DO NOT VIEW !!!
-    self.root.update_root(**tree_image)  # update the current tree with source values
-
-    # ---- decode nested dictionaries from json image to the original objects and add it to the node's list ----
-    dt = self.root.__dict__  # start from the root children
-    self._chrome_into_object(dt)  # decode nested dictionaries recursively
-
-    self._save_tree()  # save the updated current root
-
-    return True, ''
+    conv = ChromeToJSON()  # instance of (Chrome -> JSON converter)
+    res, error = conv.convert(source)  # convert and get the result
+    if res:
+        # conversion is ok, try to write the tree into destination file
+        try:
+            with open(destination, "w") as write_file:
+                json.dump(conv.root, write_file, cls=MyJSONEncoder)
+        except OSError as e:
+            res = False
+            error = str(e)
+    return res, error  # return result and error if exists
 
 
 def main():
@@ -117,28 +156,74 @@ def main():
     :return: nothing
     """
 
-ap = argparse.ArgumentParser(
-    description='File converter of Bookmark Manager database formats.'
-)
-# default max_help_position of the HelpFormatter class is 24, increase it for nice help output
-new_formatter = partial(argparse.HelpFormatter, max_help_position=28)
-ap.formatter_class = new_formatter
+    ap = argparse.ArgumentParser(
+        description='File converter of Bookmark Manager database formats.'
+    )
+    # default max_help_position of the HelpFormatter class is 24, increase it for nice help output
+    new_formatter = partial(argparse.HelpFormatter, max_help_position=28)
+    ap.formatter_class = new_formatter
 
-ap.add_argument('--version', action='version', version=version)
-ap.add_argument('--list_input_formats', action='version', version=', '.join(list_input_formats),
-                help='show list of input formats and exit')
-ap.add_argument('--list_output_formats', action='version', version=', '.join(list_output_formats),
-                help='show list of output formats and exit')
-ap.add_argument('-f', '--from', choices=list_input_formats,
-                help='source format', required=True)
-ap.add_argument('-t', '--to', choices=list_output_formats,
-                help='destination format', required=True)
-ap.add_argument('source')
-ap.add_argument('destination')
-args = ap.parse_args()
+    ap.add_argument('--version', action='version', version=version)
+    ap.add_argument('--list_input_formats', action='version', version=', '.join(list_input_formats),
+                    help='show list of input formats and exit')
+    ap.add_argument('--list_output_formats', action='version', version=', '.join(list_output_formats),
+                    help='show list of output formats and exit')
+    ap.add_argument('--verbose', action='store_true',
+                    help='switch verbose mode ON')
+    ap.add_argument('-f', '--from_', choices=list_input_formats,
+                    help='source format', required=True)
+    ap.add_argument('-t', '--to', choices=list_output_formats,
+                    help='destination format', required=True)
+    ap.add_argument('source')
+    ap.add_argument('destination')
+    args = ap.parse_args()
 
 
-print(args)
+    print(args)
+    print(type(args.verbose), args.verbose)
+
+
+    # check if source exists
+    if not os.path.isfile(args.source):
+        print_verbose(f"Source file {args.source} does not exist", args.verbose)
+        exit(1)
+
+    # check if destination already exist
+    if os.path.isfile(args.destination):
+        print(f"Destination file {args.destination} already exists.")
+        answer = input("Overwrite? [Y/n] --> ")
+        if answer.lower() != 'y':
+            print_verbose("Conversion canceled.", args.verbose)
+            exit(1)
+        else:
+            os.remove(args.destination)
+
+    # input and output files are ok
+    print(args.source)
+    print(args.destination)
+
+    # what to do
+    match args.from_, args.to:
+        case ['chrome', 'json']:  # from Chrome bookmark format to internal JSON
+            res, error = chrome_to_json(args.source, args.destination)
+        case ['chrome', 'sqlite']:  # from Chrome to internal sqlite
+            return
+        case ['json', 'sqlite']:  # from internal json to internal sqlite
+            return
+        case ['sqlite', 'json']:  # from internal sqlite to internal json
+            return
+        case _:
+            print_verbose('Invalid options.', args.verbose)
+            exit(1)
+
+    # exit section
+    # print if --verbose
+    if res:
+        print(f"{args.source} converted to {args.destination} successfully")  # success massage
+    else:
+        print_verbose(f"Error {error} occurred during conversion.", args.verbose)  # error message
+        exit(1)
+
 
 if __name__ == '__main__':
     main()
